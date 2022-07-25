021b6f8a90924eeca501b62b54c6172ac3c08ff4
Boubacar <soumanabettoboubacar@gmail.com>
2022-07-25 23:31:24 +0530

    crud operations on task

--- app.js
+++ app.js
@@ -127,6 +127,59 @@ app.delete('/tasklists/:tasklistId', (req, res) =>{
     .catch((error)=>{console.log(error)});
 });
 
+/* CRUD operations for Task, a task should always belong to a TasList */
+// Get all tasks for 1 TaskList, http://localhost:3000/taskslists/:tasklistId/tasks/:taskId
+app.get('/taskslists/:tasklistId/tasks', (req, res)=>{
+    Task.find({_taskListId: req.params.tasklistId})
+    .then((tasks)=>{
+        res.status(200).send(tasks)
+    })
+    .catch((error)=>{console.log(error)});
+});
+//Create a task inside a particular Task List
+app.post('/taskslists/:tasklistId/tasks', (req, res) =>{
+    console.log(req.body);
+
+    let taskObj = { 'title': req.body.title, '_taskListId': req.params.tasklistId};
+    Task(taskObj).save()
+        .then((task) => {
+            res.status(201).send(task);
+        })
+        .catch((error) => { 
+            console.log(error);
+            res.status(500);
+        });
+});
+
+// http://localhost:3000/taskslists/:tasklistId/tasks/:taskId
+// Get 1 task inside 1 TaskList
+app.get('/taskslists/:tasklistId/tasks/:taskId', (req, res)=>{
+    Task.findOne({_taskListId: req.params.tasklistId, _id: req.params.taskId})
+    .then((task)=>{
+        res.status(200).send(task)
+    })
+    .catch((error)=>{console.log(error)});
+});
+
+//Update 1 task belonging to 1 TaskList
+app.patch('/taskslists/:tasklistId/tasks/:taskId', (req, res) =>{
+    Task.findOneAndUpdate({ _taskListId: req.params.tasklistId, _id: req.params.taskId},{ $set: req.body})
+    .then((task)=>{
+        res.status(200).send(task)
+    })
+    .catch((error)=>{console.log(error)});
+});
+
+//Delete 1 task belonging to 1 TaskList
+app.delete('/taskslists/:tasklistId/tasks/:taskId', (req, res) =>{
+    Task.findOneAndDelete({ _taskListId: req.params.tasklistId, _id: req.params.taskId})
+    .then((task)=>{
+        res.status(200).send(task)
+    })
+    .catch((error)=>{console.log(error)});
+});
+
+
 /*app.listen(3000, function(){
     console.log("Server started on port 3000...");
 }); */
