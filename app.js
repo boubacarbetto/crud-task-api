334977861d477b3276ae4ec8e99717cb2d30df58
Boubacar <soumanabettoboubacar@gmail.com>
2022-07-30 15:41:12 +0530

    update backend app

--- app.js
+++ app.js
@@ -25,11 +25,8 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    //res.setHeader('Access-Control-Allow-Credentials', true);
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     // Pass to next layer of middleware
     next();
